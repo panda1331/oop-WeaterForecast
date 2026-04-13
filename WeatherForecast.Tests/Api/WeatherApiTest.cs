eafc344850f640814fcc5d3b870a2fad7de6b397
panda1331 <evelinamorova27@gmail.com>
2026-04-14 01:09:32 +0300

    add HandleGetCurrentWeather_WithNonNumericCoordinates_ReturnsBadRequest and HandleGetCurrentWeather_WhenControllerThrowsApiException_ReturnsInternalServerError tests

--- WeatherForecast.Tests/Api/WeatherApiTest.cs
+++ WeatherForecast.Tests/Api/WeatherApiTest.cs
@@ -8,6 +8,7 @@ using WeatherForecast.Api;
 using WeatherForecast.Controllers;
 using WeatherForecast.Models.Weather;
 using WeatherForecast.Shared.Responses;
+using WeatherForecast.Utils;
 using Xunit;
 
 namespace WeatherForecast.Tests.Api
@@ -33,5 +34,36 @@ namespace WeatherForecast.Tests.Api
             okResult.Value.Message.Should().Be("success");
             okResult.Value.Data.Temperature.Should().Be(22.5m);
         }
+
+        [Fact]
+        public async Task HandleGetCurrentWeather_WithNonNumericCoordinates_ReturnsBadRequest()
+        {
+            var latitude = "abc";
+            var longitude = "14.2345";
+            var mockController = new Mock<ICurrentWeatherController>();
+            var result = await WeatherApi.HandleGetCurrentWeather(mockController.Object, latitude, longitude);
+            var badRequestResult = result.Result.Should().BeOfType<BadRequest<Status>>().Subject;
+            badRequestResult.Value.Should().NotBeNull();
+            badRequestResult.Value!.Code.Should().Be(400);
+            badRequestResult.Value.Message.Should().Be("invalid coordinates");
+        }
+
+        [Fact]
+        public async Task HandleGetCurrentWeather_WhenControllerThrowsApiException_ReturnsInternalServerError()
+        {
+            var latitude = "53.8930";
+            var longitude = "27.5674";
+            var mockController = new Mock<ICurrentWeatherController>();
+            mockController
+                .Setup(c => c.GetCurrentWeatherAsync(It.IsAny<decimal>(), It.IsAny<decimal>()))
+                .ThrowsAsync(new ApiCallException("API is not available."));
+            var result = await WeatherApi.HandleGetCurrentWeather(mockController.Object, latitude, longitude);
+            var internalServerError = result.Result.Should().BeOfType<InternalServerError<Status>>().Subject;
+            internalServerError.Value.Should().NotBeNull();
+            internalServerError.Value!.Code.Should().Be(500);
+            internalServerError.Value.Message.Should().Be("API is not available.");
+        }
+
+
     }
 }
